6310f5fd2eaf27f3714c1a0ac600aee93b3d9ff2
joaoguilhermegrupotcm <joaoguilherme@grupotcm.com.br>
2020-12-22 16:59:14 -0300

    Primeiro Commit

---
+++ CursoMVCDIO/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVCDIO.Models
+{
+    public class Categoria
+    {
+       public int id { get; set; }
+        [Display(Name = "Descrição")]
+        public string descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ CursoMVCDIO/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVCDIO.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOtionsBuilder)
+        {
+
+            dbContextOtionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Cursomvc;Integrated Security =True;User id=joao;Password=123");
+
+        }
+
+
+    }
+}
